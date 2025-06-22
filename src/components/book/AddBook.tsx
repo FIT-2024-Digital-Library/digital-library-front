237fe7a78807e3d673b856692a50a094f77ab81e
Kozlov Kirill <kozlov.kirill.v.work@gmail.com>
2025-06-22 16:04:12 +0700

    Fix bug with wrong addr for created book page

--- src/components/book/AddBook.tsx
+++ src/components/book/AddBook.tsx
@@ -18,7 +18,7 @@ export const AddBook: React.FC = () => {
   const [, setLocation] = useLocation();
 
   const { createBook, error: createBookError } = useBookCreate((response) => {
-    setLocation(`/books/${response}`, { replace: true });
+    setLocation(`/books/${response.id}`, { replace: true });
   });
 
   return (
